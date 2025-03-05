5d8542cfdf824869b6a5cfd1c767a12705b3b8a8
ajmaestre <ajmaestretorres@gmail.com>
2025-03-05 12:12:28 -0500

    Modificando headers

--- routes/datoRoute.php
+++ routes/datoRoute.php
@@ -1,7 +1,6 @@
 <?php 
 
     header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Origin: http://dev-app:3000");
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
     header("Access-Control-Allow-Headers: Access-Control-Allow-Origin, Content-Type");
     header("Content-Type: application/json");
