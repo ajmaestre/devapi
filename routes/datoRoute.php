e26524886a60412337bf65bbd2c420a53c112715
ajmaestre <ajmaestretorres@gmail.com>
2025-03-05 13:39:20 -0500

    Modificando los headers

--- routes/datoRoute.php
+++ routes/datoRoute.php
@@ -3,6 +3,7 @@
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
     header("Access-Control-Allow-Headers: Content-Type, Authorization");
+    header("Content-Type: application/json");
 
     require_once "../app/services/datos.php";
     require_once "../app/respuestas/respuesta.php";
